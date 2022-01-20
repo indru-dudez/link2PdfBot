1cbc8b95264fdf220465623ca6b4673f204910f9
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2022-01-20 13:51:30 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config(object):
+    # Get a bot token from botfather
+    TG_BOT_TOKEN = "1944114938:AAFtYcR3cgSI4bIE3ONI6urjsrk8tpmmork"
+
+    # Get from my.telegram.org
+    APP_ID = 1555704
+
+    # Get from my.telegram.org
+    API_HASH = "f08030f122370b15dbeaefb39cb0f693"
+
